7d0971aef95297ffd18ec6d9684525e8fd131b6d
maulanayusufhabibi <maulanayusufhbb@gmail.com>
2022-03-17 11:34:50 +0700

    praprakalpro-cpp

--- kalkulator.cpp
+++ kalkulator.cpp
@@ -2,25 +2,23 @@
 #include <math.h>
 using namespace std;
 
-int hasil1,hasil2,hasil3;
-float hasil4;
 int tambah (int a, int b){
-  hasil1 = a + b;
+  int hasil1 = a + b;
   return hasil1;
 }
 
 int kurang (int c, int d){
-  hasil2 = c - d;
+  int hasil2 = c - d;
   return hasil2;
 }
 
 int kali (int e, int f){
-  hasil3 = e * f;
+  int hasil3 = e * f;
   return hasil3;
 }
 
 float bagi (int g, int h){
-  hasil4 = g / h;
+  float hasil4 = g / h;
   return hasil4;
 }
 
